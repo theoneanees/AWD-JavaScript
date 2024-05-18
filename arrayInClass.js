aae5c4ee48182afbe50d3e4bce8201854df1e71b
Anees Ur Rehman <theoneanees63@gmail.com>
2024-05-19 04:15:01 +0500

    What i wanna  descriptive commit message

--- arrayInClass.js
+++ arrayInClass.js
@@ -1,6 +1,8 @@
 let marks = [41, 50, 42, 39, 45, 34, 28]
 console.log(marks.sort());
 console.log(marks.reverse());
+console.log(marks.sort().reverse());
+console.log(marks.sort().reverse().sort());
 
 
 let larkioKaBag = ["lipstick", "makeUP", "Nail polish", "perfume", "body spray", "brush", "body Mist"]
